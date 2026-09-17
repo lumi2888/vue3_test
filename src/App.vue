97e0480b51575d5a7c0fbff452010607c3042c60
lumijhang <lumijhang@cybersoft.com.tw>
2026-09-17 18:01:17 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
-
+<!--父組件-->
 <!--setup:自動處理元件設定-->
-<script setup name="App"> // name 不寫時則為預設名
+<script lang="ts" setup name="App"> // name 不寫時則為預設名
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import Person from './components/Person.vue'
 import Practice from './components/Practice.vue'
+import {reactive} from 'vue'
+import {type  persons} from '@/types'
 
+let personlist:persons = reactive([
+  {id:'asudfysafd01',name:'張三',age:18},
+  {id:'asudfysafd02',name:'李四',age:20},
+  {id:'asudfysafd03',name:'王五',age:22},
+])
+
+console.log(personlist)
 </script>
 
 <!--寫 HTML 結構 Ex:顯示 Person 元件-->
--- src/components/Practice.vue
+++ src/components/Practice.vue
@@ -1,4 +1,4 @@
-<!--TS 介面規格約束-->
+<!--子組件-->
 <template>
     <div class="practice">
        
@@ -6,15 +6,5 @@
 </template>
 
 <script lang="ts" setup name="Practice">
-import {type personinter,type persons} from '@/types'
 
-// personinter 規格的 person 物件 - 單一物件
-// let person:personinter = {id:'asyud7asfd01',name:'張三',age:60}
-
-// 宣告 personlist，其資料型別必須符合 persons(多人列表) 的規格書要求 - 陣列物件
-let personlist:persons = [
-    {id:'asyud7asfd01',name:'張三',age:60},
-    {id:'asyud7asfd02',name:'李四',age:18},
-    {id:'asyud7asfd03',name:'王五',age:5},
-]
 </script>
